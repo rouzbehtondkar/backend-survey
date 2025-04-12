6fa0d5e1f111a118baebd508a005d7e2049e077b
rouzbehtondkar <tondkar.eng@gmail.com>
2025-04-12 13:22:42 +0330

    cors origin

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,25 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   // Enable CORS
-  app.enableCors();
-  
-  // Enable validation pipe
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    transform: true,
-  }));
-  
+  app.enableCors({
+    origin: "http://localhost:3001", // فقط این مبدا مجاز است
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    credentials: true, // اگر نیاز به ارسال کوکی یا اعتبارسنجی داشته باشید
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    })
+  );
+
   await app.listen(3000);
-  console.log('Application is running on: http://localhost:3000');
+  console.log("Application is running on: http://localhost:3000");
 }
-bootstrap(); 
+bootstrap();
